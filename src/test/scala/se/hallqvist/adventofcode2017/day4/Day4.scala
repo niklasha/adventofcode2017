4914d1ce2cf8fda4a4022cf15e00135006f3a3e9
niklas <niklas@mylifediary.com>
2017-12-05 06:46:37 +0100

    Format intital comment

--- src/test/scala/se/hallqvist/adventofcode2017/day4/Day4.scala
+++ src/test/scala/se/hallqvist/adventofcode2017/day4/Day4.scala
@@ -4,6 +4,8 @@ import org.scalatest._
 import scala.collection._
 
 /*
+--- Day 4: High-Entropy Passphrases ---
+
 A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
 
 To ensure security, a valid passphrase must contain no duplicate words.
